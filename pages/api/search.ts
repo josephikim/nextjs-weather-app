9b36670750431a6ef512b2eb821a4e683e2cc795
Joseph Kim <josephikim2@gmail.com>
2022-10-31 13:01:38 -0700

    add prettier formatting, typescript linting, husky pre-commit hook

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,20 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { forwardGeocode } from "utils/opencage";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { forwardGeocode } from 'utils/opencage'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { query } = req.query;
+const searchHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query } = req.query
 
-  const results = await getSearchResults(query as string);
+  const results = await getSearchResults(query as string)
 
-  res.status(200).json(results);
-};
+  res.status(200).json(results)
+}
 
 const getSearchResults = async (query: string) => {
   // const response = await fetch(
   //   `https://api.open-meteo.com/v1/forecast?latitude=48.8567&longitude=2.3510&hourly=temperature_2m`
   // );
 
-  const response = await forwardGeocode(query);
+  const response = await forwardGeocode(query)
 
-  return response;
-};
+  return response
+}
+
+export default searchHandler

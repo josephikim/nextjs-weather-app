4ad06547deea07f8f8fb8d2317711e8fb3a6d5d6
Joseph Kim <josephikim2@gmail.com>
2022-10-31 12:41:13 -0700

    add opencage api

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { forwardGeocode } from "utils/opencage";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { query } = req.query;
@@ -9,9 +10,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 };
 
 const getSearchResults = async (query: string) => {
-  const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=48.8567&longitude=2.3510&hourly=temperature_2m`
-  );
+  // const response = await fetch(
+  //   `https://api.open-meteo.com/v1/forecast?latitude=48.8567&longitude=2.3510&hourly=temperature_2m`
+  // );
 
-  return response.json();
+  const response = await forwardGeocode(query);
+
+  return response;
 };

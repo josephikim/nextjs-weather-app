7263ec44a8aaa648f0c2f37c2c05ca5374fbff25
Joseph Kim <josephikim2@gmail.com>
2023-03-24 23:59:27 -0700

    use grid layout on dashboard page

--- src/components/ForecastPreview.tsx
+++ src/components/ForecastPreview.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Card, Button } from 'react-bootstrap'
 import { useLocalData } from 'hooks/useLocalData'
 import { trpc } from 'utils/trpc'
-import CurrentWeather from 'components/CurrentWeather'
-
+import { degToCompass } from 'utils/weather'
+import { getWmoDescription } from 'utils/meteo'
+import TemperatureUnitSelect from 'components/TemperatureUnitSelect'
+import classes from 'styles/sass/ForecastPreview.module.scss'
 interface ForecastPreviewProps {
   label: string
   latitude: string
@@ -33,17 +35,49 @@ const ForecastPreview = ({
     return <></>
   }
 
+  const displayCondition = getWmoDescription(
+    forecast.current_weather.weathercode
+  )
+
   return (
-    <Card>
+    <Card className={classes.card}>
       <Card.Body>
         <Card.Title>{label}</Card.Title>
-        <CurrentWeather
-          current_weather={forecast.current_weather}
-          daily={forecast.daily}
-          daily_units={forecast.daily_units}
-          hourly={forecast.hourly}
-          hourly_units={forecast.hourly_units}
-        />
+        <div className={classes.flexContainer}>
+          <div className={classes.flexChild}>
+            <i
+              className={`${classes.icon} wi wi-wmo4680-${forecast.current_weather.weathercode}`}
+            ></i>
+          </div>
+          <div className={classes.flexChild}>
+            <div className={classes.alignTop}>
+              <h3 className={classes.temperature}>
+                {forecast.current_weather.temperature}
+              </h3>
+              <TemperatureUnitSelect />
+              <div>{displayCondition}</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <span className="heading">24-Hour Precipitation: </span>
+          <span>{Math.trunc(forecast.daily.precipitation_sum[0])} </span>
+          <span>{forecast.daily_units.precipitation_sum}</span>
+        </div>
+        <div>
+          <span className="heading">Humidity: </span>
+          <span>{forecast.hourly.relativehumidity_2m[0]}</span>
+          <span>{forecast.hourly_units.relativehumidity_2m}</span>
+        </div>
+        <div>
+          <span className="heading">Wind Speed: </span>
+          <span>{forecast.current_weather.windspeed} </span>
+          <span>mph</span>
+        </div>
+        <div>
+          <span className="heading">Wind Direction: </span>
+          <span>{degToCompass(forecast.current_weather.winddirection)}</span>
+        </div>
         <Button variant="primary" href={forecastRoute}>
           View Details
         </Button>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,6 +4,9 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { TouchBackend } from 'react-dnd-touch-backend'
 import update from 'immutability-helper'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import { trpc } from 'utils/trpc'
 import ForecastPreview from 'components/ForecastPreview'
 import DndMovableItem from 'components/DndMovableItem'
@@ -50,32 +53,44 @@ const DashboardPage = () => {
     <div>Loading dashboard...</div>
   )
 
+  const gridTemplate: JSX.Element = (
+    <div className="row" id="container">
+      <div className="col-4"></div>
+      <div className="col-4"></div>
+      <div className="col-4"></div>
+    </div>
+  )
+
   if (movableItems && movableItems.length < 1) {
     jsx = <span>No items found</span>
   } else if (movableItems) {
-    jsx = movableItems.map((location, index) => {
+    const items = movableItems.map((location, index) => {
       return (
-        <DndMovableItem
-          item={location}
-          index={index}
-          key={location.label}
-          moveItem={moveItem}
-        >
-          <ForecastPreview
-            label={location.label}
-            latitude={location.latitude.toString()}
-            longitude={location.longitude.toString()}
-          />
-        </DndMovableItem>
+        <Col md={4} key={index}>
+          <DndMovableItem
+            item={location}
+            index={index}
+            key={location.label}
+            moveItem={moveItem}
+          >
+            <ForecastPreview
+              label={location.label}
+              latitude={location.latitude.toString()}
+              longitude={location.longitude.toString()}
+            />
+          </DndMovableItem>
+        </Col>
       )
     })
+
+    jsx = <Row>{items}</Row>
   }
   return (
     <div className={classes.container}>
       <main className={classes.main}>
         <div className={classes.flexContainer}>
           <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
-            {jsx}{' '}
+            <Container>{jsx}</Container>
           </DndProvider>
         </div>
       </main>
